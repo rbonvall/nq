39ada9b851f6496a3d7b53b74d981d82d19b97d9
Roberto Bonvallet <rbonvall@gmail.com>
2012-12-11 18:57:29 -0300

    Implement search by pattern

--- nq.sh
+++ nq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -a extensions
+declare -a patterns
 
 for arg in $*
 do
@@ -13,9 +14,29 @@ do
             extensions+=(-or)
         fi
         extensions+=(-iname "*.$ext")
+    else
+        if [ ${#patterns[*]} -gt 0 ]
+        then
+            patterns+=(-or)
+        fi
+        patterns+=(-iname "*$arg*")
     fi
 done
 
-echo find "$PWD" ${extensions[*]}
-find "$PWD"  ${extensions[*]}
+if [ ${#extensions[*]} -gt 0 ] && [ ${#patterns[*]} -gt 0 ]
+then
+    echo find "$PWD" "(" ${extensions[*]} ")" -and "(" ${patterns[*]} ")"
+    find "$PWD" "(" ${extensions[*]} ")" -and "(" ${patterns[*]} ")"
+elif [ ${#extensions[*]} -gt 0 ]
+then
+    echo find "$PWD" ${extensions[*]}
+    find "$PWD" ${extensions[*]}
+elif [ ${#patterns[*]} -gt 0 ]
+then
+    echo find "$PWD" ${patterns[*]}
+    find "$PWD" ${patterns[*]}
+else
+    echo find "$PWD"
+    find "$PWD"
+fi
 
